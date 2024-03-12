bc39b0a6d3a0e72da217643965ddbabc713847c3
HaInseong <gkdlstjd1020@naver.com>
2024-03-12 13:06:27 +0900

    객체지향부터 다시 복습 시작

--- src/ch6/Hello2.java
+++ src/ch6/Hello2.java
@@ -3,8 +3,7 @@ package ch6;
 //하나의 소스파일에는 하나의 클래스만 작성하는 것이 바람직
 //소스파일 이름은 public class 이름과 일치해야 함
 //하나의 소스파일에는 하나의 public class만 허용함
-public class Hello2 {
+public class Hello2 {}
     class Hello3 {}
-
     class Hello4 {}
-}
+
