8ee736fd23fd8837ada81b4a69ca06d68aadf814
HaInseong <gkdlstjd1020@naver.com>
2024-03-20 20:06:41 +0900

    revision content

--- src/ch6/Ex6_14_생성자this.java
+++ src/ch6/Ex6_14_생성자this.java
@@ -44,18 +44,3 @@ public class Ex6_14_생성자this {
         System.out.println(c1.color);
     }
 }
-
-//대입 연산자로 할 수 있는 최대 범위는 배열의 생성까지
-//배열에 난수를 채우는 작업 같은 건 명시적(대입) 초기화로 할 수 없다.
-//난수로 채우는 작업은 static block에서 CV 복잡 초기화 해줘야 한다.
-/*
-class StaticBlockTest {
-    static int[] arr = new int[10];
-
-    static {
-        for(int i=0; i<arr.length; i++) {
-            arr[i] = (int)(Math.random()*10)+1;
-        }
-    }
-}
-*/
