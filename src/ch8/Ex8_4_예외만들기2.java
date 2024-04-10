8d7a03eeb4ef28fb3261834b783e41de85ea922e
HaInseong <gkdlstjd1020@naver.com>
2024-04-11 08:50:49 +0900

    checked Exception, unchecked Exception

---
+++ src/ch8/Ex8_4_예외만들기2.java
@@ -0,0 +1,12 @@
+package ch8;
+
+public class Ex8_4_예외만들기2 {
+    public static void main(String[] args) {
+        try {// Exception과 그 자손은 반드시 예외처리 해줘야 한다. 해주지 않을 시 컴파일 오류 발생한다.
+            throw new Exception();
+        } catch(Exception e) {}
+
+        // RuntimeException과 그 자손은 예외처리를 해주지 않아도 컴파일은 된다.
+        throw new RuntimeException();
+    }
+}
